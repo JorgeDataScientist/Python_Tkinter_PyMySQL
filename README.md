8fbb8a6948dd7ba8296fbd7e0093f8e51c57f047
Jorge Luis <Desarrollador@tutanota.com>
2023-04-20 13:09:29 -0500

    Creando Frontend
    Creando Frontend con Tkinter:
    Ventana
    Etiquetas
    Cuadros de Dialogo
    Botones

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Python_Tkinter_PyMySQL
+
 Crea Tabla (CREATE), Inserta (INSERT) y Elimina (DELETE)
+
+En este proyecto, he desarrollado una aplicación de registro de datos de usuario utilizando Python y la librería de GUI Tkinter. Esta aplicación permite a los usuarios ingresar sus datos personales como nombre, correo electrónico, dirección y número de teléfono en un formulario de registro fácil de usar.
+
+Una vez que se completa el formulario, los datos del usuario se guardan en una base de datos MySQL utilizando la librería PyMySQL, lo que permite una fácil gestión y acceso a la información registrada.
+
+El diseño de la interfaz de usuario es intuitivo y fácil de navegar, lo que permite a los usuarios ingresar sus datos de manera rápida y sencilla. Además, la aplicación ofrece validación de datos para garantizar que los campos estén completos y sean precisos antes de guardar la información en la base de datos.
+
+En general, esta aplicación es una herramienta útil para cualquier organización que necesite registrar y gestionar datos de usuario de manera eficiente y efectiva. La combinación de Python, Tkinter y PyMySQL permite crear una solución simple y poderosa para el registro de datos de usuario.
---
+++ Virtual_Environment/Creando_Ventana.py
@@ -0,0 +1,87 @@
+import tkinter as tk
+
+# Crear la ventana
+# -------------------------------------------------------------
+ventana = tk.Tk()
+# Agregar título
+ventana.title("Formulario de Registro")
+# Establecer tamaño de la ventana
+ventana.geometry("290x600")
+# No permitir cambiar el tamaño de la ventana
+ventana.resizable(False, False)
+# Color de Fondo
+ventana.configure(background="light cyan")
+# Cargamos una imagen utilizando el método PhotoImage() y especificando la ruta de la imagen en el argumento file
+imagen = tk.PhotoImage(file='../Imagenes/tkinter.png')
+# Utilizamos el método subsample() para reducir el tamaño de la imagen por un factor de 6
+imagen = imagen.subsample(6, 6)
+# Creamos una etiqueta con el método Label() y especificando la imagen en el argumento image y el color de fondo de la imagen
+label = tk.Label(image=imagen, bg="light cyan")
+# Empaquetamos la etiqueta dentro de la ventana con el método pack()
+label.place(x=1, y=1)
+
+# Crear Etiquetas y Cuadros de Textos
+# --------------------------------------------------------------
+# Insertando etiquetas 'Id'
+e0 = tk.Label(ventana, text='Id', bg='gray', fg='white')
+e0.place(x=10, y=imagen.height() + 20)
+# Cuadro de texto que almacena el 'Nombre'
+ed0 = tk.Entry(ventana, width=23)
+ed0.place(x=10, y=imagen.height() + 50)
+
+# Insertando etiquetas 'Nombre'
+e1 = tk.Label(ventana, text='Nombre', bg='gray', fg='white')
+e1.place(x=10, y=imagen.height() + 90)
+# Cuadro de texto que almacena el 'Nombre'
+ed1 = tk.Entry(ventana, width=23)
+ed1.place(x=10, y=imagen.height() + 120)
+
+# Insertando etiquetas 'Apellido Paterno'
+e2 = tk.Label(ventana, text='Apellido Paterno', bg='gray', fg='white')
+e2.place(x=10, y=imagen.height() + 160)
+# Cuadro de texto que almacena el 'Apellido Paterno'
+ed2 = tk.Entry(ventana, width=23)
+ed2.place(x=10, y=imagen.height() + 190)
+
+# Insertando etiquetas 'Apellido Materno'
+e3 = tk.Label(ventana, text='Apellido Materno', bg='gray', fg='white')
+e3.place(x=10, y=imagen.height() + 230)
+# Cuadro de texto que almacena el 'Apellido Materno'
+ed3 = tk.Entry(ventana, width=23)
+ed3.place(x=10, y=imagen.height() + 260)
+
+# Insertando etiquetas 'Cedula de Identidad'
+e3 = tk.Label(ventana, text='Cedula de Identidad', bg='gray', fg='white')
+e3.place(x=10, y=imagen.height() + 300)
+# Cuadro de texto que almacena el 'Cedula de Identidad'
+ed3 = tk.Entry(ventana, width=23)
+ed3.place(x=10, y=imagen.height() + 330)
+
+# Insertando etiquetas 'Email'
+e3 = tk.Label(ventana, text='Email', bg='gray', fg='white')
+e3.place(x=10, y=imagen.height() + 370)
+# Cuadro de texto que almacena el 'Email'
+ed3 = tk.Entry(ventana, width=23)
+ed3.place(x=10, y=imagen.height() + 400)
+
+# Crear Botones
+# -------------------------------------------------------------
+# Insertando Boton 'Registrar'
+boton1 = tk.Button(ventana, text='Registrar', fg='Black', width=15)
+boton1.place(x=10, y=imagen.height() + 440)
+
+# Insertando Boton 'Consultar'
+boton2 = tk.Button(ventana, text='Consultar', fg='Black', width=15)
+boton2.place(x=boton1.winfo_x() + boton1.winfo_width() + 150, y=imagen.height() + 440)
+
+# # Insertando Boton 'Actualizar'
+boton3 = tk.Button(ventana, text='Actualizar', fg='Black', width=15)
+boton3.place(x=10, y=imagen.height() + 470)
+
+# # Insertando Boton 'Eliminar'
+boton4 = tk.Button(ventana, text='Eliminar', fg='Black', width=15)
+boton4.place(x=boton3.winfo_x() + boton3.winfo_width() + 150, y=imagen.height() + 470)
+
+# -------------------------------------------------------------
+# Ejecutar el bucle principal de eventos
+ventana.mainloop()
